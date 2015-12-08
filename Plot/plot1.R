88a01cf00b6642f8d7255fd4d0691fc556ec6b0a
nini4487 <nicomichel@hotmail.ch>
2015-12-08 22:09:42 +0100

    Code Plote1

--- Plot/plot1.R
+++ Plot/plot1.R
@@ -1,1 +1,11 @@
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings ="?", dec=".")
+str(data)
 
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+startDate <- as.Date("01/02/2007", format="%d/%m/%Y")
+endDate <- as.Date("02/02/2007", format="%d/%m/%Y")
+SSdata <- data[data$Date >= startDate & data$Date <= endDate,]
+
+png("plot1.png", width=480, height=480)
+hist(SSdata$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
