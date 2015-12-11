80e3b3565fbd584186692e8a05db59dc7e5ba5db
nini4487 <nicomichel@hotmail.ch>
2015-12-11 06:24:31 +0100

    plot 4

--- plot4.R
+++ plot4.R
@@ -10,15 +10,15 @@ SSdata <- data[data$DateDay >= startDate & data$DateDay <= endDate,]
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
 
-plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(SSdata$DateDay, SSdata$Global_active_power, type="l", xlab="", ylab="Global Active Power", cex=0.2)
 
-plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(SSdata$DateDay, SSdata$Voltage, type="l", xlab="datetime", ylab="Voltage")
 
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(SSdata$DateDay, SSdata$Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
+lines(SSdata$DateDay, SSdata$Sub_metering_2, type="l", col="red")
+lines(SSdata$DateDay, SSdata$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=1, col=c("black", "red", "blue"), bty="n")
 
-plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+plot(SSdata$DateDay, SSdata$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
